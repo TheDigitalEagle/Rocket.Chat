ca8f14811638f29496e5f92338dfb93176eb43ca
Chad Kullhem <TheDigitalEagle@users.noreply.github.com>
2021-10-20 12:16:58 -0600

    Update beforeCreateRoom.js
    Ignore E2E settings check and enforce encryption --testing

--- app/e2e/server/beforeCreateRoom.js
+++ app/e2e/server/beforeCreateRoom.js
@@ -2,10 +2,15 @@ import { callbacks } from '../../callbacks/server';
 import { settings } from '../../settings/server';
 
 callbacks.add('beforeCreateRoom', ({ type, extraData }) => {
-	if (
-		settings.get('E2E_Enabled') && ((type === 'd' && settings.get('E2E_Enabled_Default_DirectRooms'))
-		|| (type === 'p' && settings.get('E2E_Enabled_Default_PrivateRooms')))
-	) {
-		extraData.encrypted = extraData.encrypted ?? true;
+	//if (
+//		settings.get('E2E_Enabled') && ((type === 'd' && settings.get('E2E_Enabled_Default_DirectRooms'))
+//		|| (type === 'p' && settings.get('E2E_Enabled_Default_PrivateRooms')))//
+//	) {
+//		extraData.encrypted = extraData.encrypted ?? true;
+//	}
+	
+	if(type === 'd' || type === 'p')
+	{
+		extraData.encrypted = true;
 	}
 });
